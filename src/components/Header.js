bc621cefb6d5474b0c5ecf80800a1cd776544ecc
Seth Mitchell <sayotte.mitchell@gmail.com>
2023-02-13 23:18:29 -0500

    Misc + mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,54 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useLocation } from 'react-router-dom'
+import { IoIosMenu } from 'react-icons/io';
+import { IoCloseOutline } from 'react-icons/io5';
 
 const Header = () => {
 
-  const location = useLocation(); 
+    const location = useLocation(); 
+
+    const [ expandedMenu, setExpandedMenu ] = useState(false);
+    const [ isDesktop, setIsDesktop ] = useState(true);
+
+    const updateWidth = () => {
+        setIsDesktop(window.innerWidth > 740);
+    }
+
+    window.addEventListener("resize", updateWidth);
+
+    const toggleMenu = () => {
+        setExpandedMenu(!expandedMenu);
+    }
+
+    useEffect(() => {
+        updateWidth();
+    }, [])
     
   return (
-    <div className='header'>
-        <Link to='/'>
-            <h1>Seth Mitchell</h1>
-        </Link>
-        <nav>
-            <Link to="/resume" className={location.pathname.includes('resume') ? 'active' : ''}>resume</Link>
-            <Link to="/work" className={location.pathname.includes('work') ? 'active' : ''}>work</Link>
-            <Link to="/contact" className={location.pathname.includes('contact') ? 'active' : ''}>contact</Link>
-        </nav>
+    <div className={(isDesktop || !expandedMenu) ? 'header' : 'header expanded'}>
+        <div className='header-content'>
+            <Link to='/'>
+                <h1>Seth Mitchell</h1>
+            </Link> 
+            <section className={expandedMenu ? 'close' : ''}>
+                {
+                    isDesktop ?
+                    null :
+                    <div className='mobile-menu-toggle'>
+                        {
+                            expandedMenu ?
+                            <IoCloseOutline onClick={toggleMenu} /> :
+                            <IoIosMenu onClick={toggleMenu} />
+                        }
+                    </div>
+                }
+                <nav className={!isDesktop ? expandedMenu ? 'mobile open' : 'mobile' : 'desktop'}>
+                    <Link to="/resume" onClick={!isDesktop ? toggleMenu : null} className={location.pathname.includes('resume') ? 'active' : ''}>resume</Link>
+                    <Link to="/work" className={location.pathname.includes('work') ? 'active' : ''}>work</Link>
+                    <Link to="/contact" className={location.pathname.includes('contact') ? 'active' : ''}>contact</Link>
+                </nav>
+            </section>
+        </div>
     </div>
   )
 }
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -9,27 +9,61 @@ const Landing = ({isMobileSafari}) => {
 
     const [tileExpand, setTileExpand ] = useState('');
 
-    const toggleTile = (slug, rowIndex, item) => {
-        if (tileExpand === '') {
+    let isOpen = false;
+    let lastOpened;
+    let lastIndex;
+
+    const toggleTile = (slug, rowIndex) => {
+        console.log(lastOpened, lastIndex);
+        console.log(slug, isOpen);
+        if (!lastOpened) {  
+            console.log(document.querySelector(`.row${rowIndex}`));
+            document.querySelector(`.row${rowIndex}`).classList.toggle(`expanded-${slug}`);
+        } else if (lastOpened === slug) {
+            console.log(document.querySelector(`.row${rowIndex}`));
+            document.querySelector(`.row${rowIndex}`).classList.toggle(`expanded-${slug}`);
+        } else if (isOpen) {
+            document.querySelector(`.row${lastIndex}`).classList.toggle(`expanded-${lastOpened}`);
             document.querySelector(`.row${rowIndex}`).classList.toggle(`expanded-${slug}`);
         } else {
             document.querySelector(`.row${rowIndex}`).classList.toggle(`expanded-${slug}`);
         }
-        
+        // updateTileStatus(slug);
+        if (document.querySelector(`.row${rowIndex}`).classList.contains(`expanded-${slug}`)) {
+            isOpen = true;
+        } else {
+            isOpen = false;
+        }
+        lastOpened = slug;
+        lastIndex = rowIndex;
+    }
+
+    const updateTileStatus = (slug) => {
+        if (tileExpand === '') {
+            setTileExpand(slug);
+        } else if (lastOpened === slug) {
+            setTileExpand('');
+        } else {
+            setTileExpand(slug);
+        }
     }
 
     const RenderTileRow = ({row, parentIndex}) => {
         return (
             <div className={'project-row' + ' ' + "row"+parentIndex}>
-                <div className='tile' id={row[0].content.slug} onClick={() => toggleTile(row[0].content.slug, parentIndex, 0)} style={{backgroundColor: row[0].color}} >
-                    <h2 style={{color: row[0].fontColor, fontFamily: row[0].font, fontWeight: row[0].weight}}>{row[0].title}</h2>
-
-                    <BsPlusCircleFill className={tileExpand === row[0].content.slug ? 'tile-toggle close' : 'tile-toggle'} style={{color: row[0].toggleColor}} />
+                <div className='tile' id={row[0].content.slug} style={{backgroundColor: row[0].color}} >
+                    <div className='tile-header'>
+                        
+                        <h2 style={{color: row[0].fontColor, fontFamily: row[0].font, fontWeight: row[0].weight}}>{row[0].title}</h2>
+                    </div>
+                    <BsPlusCircleFill className='tile-toggle' onClick={() => toggleTile(row[0].content.slug, parentIndex)} style={{color: row[0].toggleColor}} />
                 </div>
                 <div className='tile' id={row[1].content.slug} style={{backgroundColor: row[1].color}} >
-                    <h2 style={{color: row[1].fontColor, fontFamily: row[1].font, fontWeight: row[1].weight}}>{row[1].title}</h2>
+                    <div className='tile-header'>
 
-                    <BsPlusCircleFill className={tileExpand === row[1].content.slug ? 'tile-toggle close' : 'tile-toggle'} onClick={() => toggleTile(row[1].content.slug, parentIndex, 1)} style={{color: row[1].toggleColor}} />
+                        <h2 style={{color: row[1].fontColor, fontFamily: row[1].font, fontWeight: row[1].weight}}>{row[1].title}</h2>
+                    </div>
+                    <BsPlusCircleFill className='tile-toggle' onClick={() => toggleTile(row[1].content.slug, parentIndex, 1)} style={{color: row[1].toggleColor}} />
                 </div>
             </div>
         )
