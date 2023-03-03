f93181c31b41febe8ad33fbee54ca6d42ec97380
Seth Mitchell <sayotte.mitchell@gmail.com>
2023-03-02 23:23:23 -0500

    Couple Updates

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { IoRocketSharp } from 'react-icons/io5';
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='not-found'>
+      <h2>404</h2>
+      <div>Hmm... what you're looking for doesn't seem to exist.</div>
+      <Link to='/'><IoRocketSharp /> take me somewhere else</Link>
+    </div>
   )
 }
 
