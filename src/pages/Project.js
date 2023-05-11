c30b79859fb38b4b25bb7e7e1cf1cadf858f79c1
The Blocks <127040676+the-blocks@users.noreply.github.com>
2023-05-10 21:36:39 -0400

    Work on project page structure

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, createElement } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import * as TbIcon from 'react-icons/tb';
 import * as IoIcon from 'react-icons/io5';
+import * as FaIcon from 'react-icons/fa';
 import { FaIdBadge } from 'react-icons/fa';
 import { BsAppIndicator } from 'react-icons/bs';
 
@@ -26,6 +27,8 @@ const Project = ({defaultDark}) => {
             icon = React.createElement(IoIcon[iconName]);
         } else if (iconName.slice(0, 2) === 'Tb') {
             icon = React.createElement(TbIcon[iconName]);
+        } else if (iconName.slice(0, 2) === 'Fa') {
+            icon = React.createElement(FaIcon[iconName]);
         }
         
         return (
@@ -48,17 +51,17 @@ const Project = ({defaultDark}) => {
                     </div>
                     <div className='status-info-chips'>
                         <div>
-                            <span className='project-info-chip' style={{backgroundColor: `var(${project.content.tileColor})`}}>{project.content.technologies}</span>
+                            <span className='project-info-chip' style={{backgroundColor: `${project.content.tileColor}`}}>{project.content.technologies}</span>
                             {/* <h4>TECH</h4> */}
                             <TbIcon.TbCode />
                         </div>
                         <div>
-                            <span className='project-info-chip' style={{backgroundColor: `var(${project.content.tileColor})`}}>{project.content.role}</span>
+                            <span className='project-info-chip' style={{backgroundColor: `${project.content.tileColor}`}}>{project.content.role}</span>
                             {/* <h4>ROLE</h4> */}
                             <TbIcon.TbIdBadge />
                         </div>
                         <div>
-                            <span className='project-info-chip' style={{backgroundColor: `var(${project.content.tileColor})`}}>{project.content.status}</span>
+                            <span className='project-info-chip' style={{backgroundColor: `${project.content.tileColor}`}}>{project.content.status}</span>
                             {/* <h4>STATUS</h4> */}
                             <TbIcon.TbCircleDashed />
                         </div>
@@ -75,7 +78,7 @@ const Project = ({defaultDark}) => {
                                 console.log(project.content.tileColor);
                                
                                 return (
-                                    <div className='highlight-tile' key={index} style={{backgroundColor: `var(${project.content.tileColor})`}}>
+                                    <div className='highlight-tile' key={index} style={{backgroundColor: `${project.content.tileColor}`}}>
                                         <Icon iconName={item.icon} />
                                         <span>{item.label}</span>
                                     </div>
@@ -86,14 +89,47 @@ const Project = ({defaultDark}) => {
                 </section>
                 <section className='project-content'>
                     <div className='description'>
-
+                        <div style={{color: `${project.content.tileColor}`}} class='proj-section-header'>
+                            <Icon iconName={project.content.descriptionIcon} />
+                            <label style={{color: `${project.fontColor}`}}>{project.content.descriptionHeader}</label>
+                        </div>
+                        {project.content.description}
                     </div>
                     <div className='visuals'>
-
+                        <div style={{color: `${project.content.tileColor}`}} class='proj-section-header'>
+                            <Icon iconName={project.content.visualsIcon} />
+                            <label style={{color: `${project.fontColor}`}}>{project.content.visualsHeader}</label>
+                        </div>
+                        <sl-carousel class="scroll-hint" navigation loop>
+                            <span style={{color: `${project.fontColor}`}} slot="previous-icon">
+                                <TbIcon.TbSquareRoundedArrowLeftFilled />
+                            </span>
+                            {
+                                project.content.visuals.map((item, index) => {
+                                    return (
+                                        <sl-carousel-item>
+                                            <img src={'.' + item} key={index} style={{borderColor: `${project.content.tileColor}`}} />
+                                        </sl-carousel-item>
+                                    )
+                                })
+                            }
+                            <span style={{color: `${project.fontColor}`}} slot="next-icon">
+                                <TbIcon.TbSquareRoundedArrowRightFilled />
+                            </span>
+                        </sl-carousel>
+                        
                     </div>
-                    <div className='roadmap'>
-
-                    </div>
+                    {
+                        project.content.hasRoadmap ?
+                        <div className='roadmap'>
+                            <div style={{color: `${project.content.tileColor}`}} class='proj-section-header'>
+                                <Icon iconName={project.content.roadmapIcon} />
+                                <label style={{color: `${project.fontColor}`}}>{project.content.roadmapHeader}</label>
+                            </div>
+                        </div> :
+                        null
+                    }
+                    
                 </section>
             </div>
             
